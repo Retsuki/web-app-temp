6d95548aaebf9f23688e8e3ec6ee69ad02c75a59
Retsuki <retu1118@gmail.com>
2025-06-27 07:56:16 +0900

    feat: フォームコンポーネントとラベルコンポーネントの実装を追加

---
+++ web/src/components/app/input/form-input.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import * as React from 'react'
+import { Control, FieldPath, FieldValues } from 'react-hook-form'
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { cn } from '@/lib/utils'
+
+interface FormInputProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> extends Omit<React.ComponentProps<'input'>, 'name' | 'value' | 'onChange' | 'onBlur' | 'ref'> {
+  control: Control<TFieldValues>
+  name: TName
+  label?: string
+  description?: string
+  containerClassName?: string
+}
+
+export function FormInput<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({
+  control,
+  name,
+  label,
+  description,
+  containerClassName,
+  className,
+  ...props
+}: FormInputProps<TFieldValues, TName>) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className={containerClassName}>
+          {label && <FormLabel>{label}</FormLabel>}
+          <FormControl>
+            <Input
+              {...field}
+              {...props}
+              className={cn(className)}
+              value={field.value ?? ''}
+            />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
