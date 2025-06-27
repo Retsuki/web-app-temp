c71605297abaad860bd9057d236b528626cc82d5
Retsuki <retu1118@gmail.com>
2025-06-27 09:31:09 +0900

    style: Mindaroカラーパレットを導入し、プライマリーボタンのスタイルを更新

--- web/src/components/app/button/primary-button.tsx
+++ web/src/components/app/button/primary-button.tsx
@@ -1,9 +1,9 @@
+import type { VariantProps } from "class-variance-authority";
 import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { type VariantProps } from "class-variance-authority";
 
-export interface PrimaryButtonProps 
+export interface PrimaryButtonProps
   extends React.ComponentProps<"button">,
     VariantProps<typeof Button> {
   asChild?: boolean;
@@ -18,8 +18,8 @@ export const PrimaryButton = React.forwardRef<
       ref={ref}
       variant={variant}
       className={cn(
-        "bg-indigo-600 hover:bg-indigo-700 text-white",
-        "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
+        "text-white",
+        "focus:outline-none focus:ring-2 focus:ring-offset-2",
         "disabled:opacity-50 disabled:cursor-not-allowed",
         className
       )}
