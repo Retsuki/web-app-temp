f5da4cc90b0108a876eed6185e2ee11e9abc099d
Retsuki <retu1118@gmail.com>
2025-09-14 18:12:50 +0900

    feat: Add drizzle configuration for database setup

---
+++ api/drizzle.config.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv'
+import { defineConfig } from 'drizzle-kit'
+
+dotenv.config({ path: '../.env' })
+
+export default defineConfig({
+  schema: './src/drizzle/db/drizzle-kit.ts',
+  out: './src/drizzle/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:54322/postgres',
+  },
+  verbose: true,
+  strict: false,
+})
